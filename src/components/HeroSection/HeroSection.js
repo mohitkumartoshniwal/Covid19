49a69c592ca1d7f5b82e75892fa24d6ab2343cde
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2021-11-26 00:54:00 +0530

    initial

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -10,8 +10,8 @@ function HeroSection() {
                 <article className="hero__description textAlignment">
                     <h2 className="hero__description__subtitle ">COVID-19 Alert</h2>
                     <h1 className="hero__description__title">
-                        Stay at Home quarantine
-                        To stop Corona virus</h1>
+                        Stay At Home Quarantine
+                        To Stop Corona Virus</h1>
                     <p className="hero__description__content">
                         There is no specific medicine to prevent or treat coronavirus disease (COVID-19). People may need supportive care to .
                     </p>
