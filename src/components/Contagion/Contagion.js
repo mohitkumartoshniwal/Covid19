eb9f43f08369efbf49845b1030ac04d5d462abdb
mohitkumartoshniwal <mohitkrtoshniwal@gmail.com>
2021-11-26 23:04:15 +0530

    initial

--- src/components/Contagion/Contagion.js
+++ src/components/Contagion/Contagion.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from '../Card/Card'
+import Card from '../subComponents/Card/Card'
 import './Contagion.css'
 
 let data = [
--- src/components/Prevention/Prevention.js
+++ src/components/Prevention/Prevention.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Row from './Row/Row'
+import Row from '../subComponents/Row/Row'
 import './Prevention.css'
 let data = [
     {
